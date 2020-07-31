40c0a57dd41557e73bdbb2b5b1cab52cec3ebb7e
kabor <kabor@LAPTOP-1M107DHK.(none)>
2020-07-31 21:27:20 +0200

    gestion de la base de donnée

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +13,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class PropertyController extends AbstractController
 {
     /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $manager;
+
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $manager)
+    {
+        $this->repository = $repository;
+        $this->manager = $manager;
+    }
+    /**
      * @Route("/biens", name="property.index")
      * @return Response
      */
     public function index():Response
     {
+
         return $this->render('property/index.html.twig', [
-            'current_menu' =>  'properties'
+            'current_menu' => 'properties'
         ]);
 
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     * @return Response
+     */
+    public function show($slug, $id): Response
+    {
+        $property= $this->repository->find($id);
+        return $this->render('property/show.html.twig', [
+            'property'=> $property,
+            'current_menu'=> 'properties'
+        ]);
     }
 }
